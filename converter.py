0d7fd7e362a1136a52a270950654993127a5d79c
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2022-11-04 15:39:11 +0100

    omo :(

--- converter.py
+++ converter.py
@@ -7,16 +7,12 @@ with open ("NFT Naming csv - All Teams.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
     data = []
+    list = []
     for row in reader:
-        row[6] = []
-        for rows in row[6]:
-            row[6].append({"hair":rows[0], "eyes":rows[1], "teeth":rows[2], "clothing":rows[3], "accessories":rows[4], "expression":rows[5], "strength":rows[6], "weakness":rows[7]})
-        data.append({"TEAM NAMES":row[0],"SerialNumber":row[1], "FileName":row[2], "Name":row[3], "Description":row[4], "Gender":row[5], "Attributes":row[6], "UUID":row[7]})
-        # attributes = row["Attributes"]
-        # attributes = []
-        # for row in attributes:
-        #     attributes.append({"hair":row[0], "eyes":row[1], "teeth":row[2], "clothing":row[3], "accessories":row[4], "expression":row[5], "strength":row[6], "weakness":row[7]})
-
+        split = row[6].split(";")
+        for items in split:
+            item = items.split(":")
+        data.append({"format":"CHIP-0007","name":row[2],"description":row[4], "minting_tool":row[0], "sensitive_content":False, "series_number":row[1], "series_total":420, "gender":row[5], "Attributes":item, "UUID":row[7]})
 #Converting the NFT.csv file to JSON
 with open ("NFT Naming.json", "w") as f:
     json.dump(data, f, indent=3)
